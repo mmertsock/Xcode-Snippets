c04cfd376a3630cbd48626863b8383d11b87ddb2
mmertsock <mike.mertsock@gmail.com>
2013-09-11 08:44:26 -0400

    Add lazy-loaded property snippet

---
+++ lazyprop.m
@@ -0,0 +1,8 @@
+// Lazy-loaded properties
+// Use when lazy-loading ivars inside property accessors
+//
+// Platform: All
+// Language: Objective-C
+// Completion Scope: Function or Method
+
+return <#ivar#> ?: (<#ivar#> = <#expression#>);
