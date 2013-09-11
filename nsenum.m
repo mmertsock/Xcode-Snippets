cb122e218236270ab0c25f267919c02143d3096d
mmertsock <mike.mertsock@gmail.com>
2013-09-11 08:43:35 -0400

    Add 'typedef NS_ENUM' snippet

---
+++ nsenum.m
@@ -0,0 +1,10 @@
+// typedef NS_ENUM
+// A proper NS_ENUM typedef declaration for enums
+//
+// Platform: All
+// Language: Objective-C
+// Completion Scope: Top Level
+
+typedef NS_ENUM(<#integralType#>, <#enumName#>) {
+    <#firstValue#>
+};
